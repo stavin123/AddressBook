1b91d8426ea19915c488612e7f0b79aac3a5a680
Stavin Alban <stavinalban@gmail.com>
2024-01-24 23:07:59 +0530

    Feature Added: Created Address Book

--- AddressBookService.java
+++ AddressBookService.java
@@ -1,2 +1,5 @@
 public class AddressBookService {
+    public String display(Person p){
+        return p.getFirstName()+" "+p.getLastName()+" "+p.getCity()+" "+p.getState()+" "+ p.getEmail()+" "+p.getPhoneNumber()+ " "+p.getZip();
+    }
 }
