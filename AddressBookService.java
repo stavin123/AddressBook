662cee0f068ee583121025f1e616989cf06c9d8a
Stavin Alban <stavinalban@gmail.com>
2024-01-24 23:25:48 +0530

    Feature Added: Adding details Address Book

--- AddressBookService.java
+++ AddressBookService.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class AddressBookService {
     public String display(Person p){
         return p.getFirstName()+" "+p.getLastName()+" "+p.getCity()+" "+p.getState()+" "+ p.getEmail()+" "+p.getPhoneNumber()+ " "+p.getZip();
+    }
+
+    public void setValues(Person p) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first name");
+        String fn = sc.next();
+        p.setFirstName(fn);
+        System.out.println("Enter last name");
+        String ln = sc.next();
+        p.setLastName(ln);
+        System.out.println("Enter city");
+        String city = sc.next();
+        p.setCity(city);
+        System.out.println("Enter state");
+        String state = sc.next();
+        p.setState(state);
+        System.out.println("Enter email");
+        String email = sc.next();
+        p.setEmail(email);
+        System.out.println("Enter phn no");
+        int phNumber = sc.nextInt();
+        p.setPhoneNumber(phNumber);
+        System.out.println("Enter zip");
+        int zip = sc.nextInt();
+        p.setZip(zip);
     }
 }
--- Main.java
+++ Main.java
@@ -2,6 +2,12 @@ public class Main {
     public static void main(String[] args) {
         Person p1=new Person();
         AddressBookService s1 =new AddressBookService();
+        AddressBookHashFunction a1=new AddressBookHashFunction();
+        s1.setValues(p1);
         System.out.println(s1.display(p1));
+        a1.contactList.put(6,p1);
+        System.out.println(a1.contactList);
+
+
     }
 }
